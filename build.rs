d984be4adffd1cb90ebaf4e5cff606e8b38a7cea
nmojica <nmojica@signalsecure.com>
2019-11-19 10:18:42 -0600

    Proper Cargo.toml and build.rs for generating the lib.rs bindings on compile-time and linking to the pre-installed Liquid-dsp C library

--- build.rs
+++ build.rs
@@ -1,16 +1,61 @@
-extern crate bindgen;
-
 use std::env;
-use std::path::PathBuf;
+use std::env::consts;
+use std::path::Path;
 
-fn main(){
-    println!("cargo:rustc-link-lib=liquid");
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
+use std::fs::File;
+use std::io::Write;
+
+fn format_write(builder: bindgen::Builder, output: &str) {
+    let s = builder
         .generate()
-        .expect("Unable to generate bindings!");
+        .unwrap()
+        .to_string()
+        .replace("/**", "/*")
+        .replace("/*!", "/*");
 
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings.write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+    let mut file = File::create(output).unwrap();
+
+    let _ = file.write(s.as_bytes());
+}
+
+fn common_builder() -> bindgen::Builder {
+    bindgen::builder()
+        .raw_line("#![allow(deprecated)]")
+        .raw_line("#![allow(dead_code)]")
+        .raw_line("#![allow(non_camel_case_types)]")
+        .raw_line("#![allow(non_snake_case)]")
+        .raw_line("#![allow(non_upper_case_globals)]")
+}
+
+fn main() {
+
+    let builder = common_builder()
+        .header("wrapper.h");
+
+    format_write(builder, "src/lib.rs");
+
+    if let Some(lib_dir) = env::var_os("LIQUID_LIB_DIR") {
+
+        let lib_dir = Path::new(&lib_dir);
+        let dylib_name;
+        let target = env::var("TARGET").unwrap();
+
+        if target.contains("windows") {
+            dylib_name = "liquid.lib".to_owned();
+        } else {
+            dylib_name = format!("{}liquid{}", consts::DLL_PREFIX, consts::DLL_SUFFIX);
+        }
+
+        if lib_dir.join(dylib_name).exists() {
+            println!("cargo:rustc-link-search=native={}", lib_dir.display());
+            println!("cargo:rustc-link-lib=liquid");
+            return;
+        }
+
+        println!("cargo:warning=library not found in {}", lib_dir.display());
+        println!("export LIQUID_LIB_DIR with the path to the liquid library, for example ");
+        println!("export LIQUID_LIB_DIR=/usr/lib  ");
+
+        panic!();
+    }
 }
