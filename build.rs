ca837532fabc66743d997e13b17a117da5c016a1
porkiedev <porkiedev@gmail.com>
2024-02-08 22:56:05 -0600

    Update build.rs and remove wrapper file

--- build.rs
+++ build.rs
@@ -1,16 +1,23 @@
 extern crate bindgen;
-
 use std::env;
 use std::path::PathBuf;
 
 fn main(){
-    println!("cargo:rustc-link-lib=liquid");
+    // If on windows, search in the `./liquid` dir for the libliquid.a file
+    // NOTE: On windows, you should be buildling the static library yourself under MingW64 (or similar). Put the `libliquid.a` file in the `./liquid` dir manually.
+    #[cfg(target_os="windows")]
+    println!("cargo:rustc-link-search={}", PathBuf::from("liquid").canonicalize().unwrap().to_str().unwrap());
+    
+    // Link the liquid library
+    println!("cargo:rustc-link-lib=static=liquid");
+
     let bindings = bindgen::Builder::default()
-        .header("wrapper.h")
+        .generate_comments(true)
+        .header("liquid/liquid.h")
         .generate()
-        .expect("Unable to generate bindings!");
+        .expect("Failed to generate bindings");
 
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings.write_to_file(out_path.join("bindings.rs"))
-        .expect("Couldn't write bindings!");
+        .expect("Failed to write bindings");
 }
--- wrapper.h
+++
@@ -1,1 +0,0 @@
-#include <liquid/liquid.h>
