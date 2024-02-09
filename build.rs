37e3ecc1c8fe80795c8893b58c2c1ce86e480411
porkiedev <porkiedev@gmail.com>
2024-02-08 23:37:06 -0600

    Add support for user-provided OR system-provided lib and header files

--- build.rs
+++ build.rs
@@ -3,20 +3,30 @@ use std::env;
 use std::path::PathBuf;
 
 fn main(){
-    // If on windows, search in the `./liquid` dir for the libliquid.a file
-    // NOTE: On windows, you should be buildling the static library yourself under MingW64 (or similar). Put the `libliquid.a` file in the `./liquid` dir manually.
-    #[cfg(target_os="windows")]
-    println!("cargo:rustc-link-search={}", PathBuf::from("liquid").canonicalize().unwrap().to_str().unwrap());
-    
-    // Link the liquid library
-    println!("cargo:rustc-link-lib=liquid");
+    // Use user-provided header and lib files in the `liquid` subdirectory if provided
+    // Otherwise use the system-provided header and lib
+    let header = match std::fs::metadata("liquid/liquid.h") {
+        // `liquid/liquid.h` exists. Use the user-provided static lib
+        Ok(_metadata) => {
+            println!("cargo:rustc-link-search={}", PathBuf::from("liquid").canonicalize().unwrap().to_str().unwrap());
+            println!("cargo:rustc-link-lib=static=liquid");
+            "liquid/liquid.h"
+        },
+        // Use the system-provided header and lib
+        Err(_err) => {
+            println!("cargo:rustc-link-lib=liquid");
+            "wrapper.h"
+        }
+    };
 
+    // Generate the bindings
     let bindings = bindgen::Builder::default()
-        .generate_comments(true)
-        .header("liquid/liquid.h")
-        .generate()
-        .expect("Failed to generate bindings");
+    .generate_comments(true)
+    .header(header)
+    .generate()
+    .expect("Failed to generate bindings");
 
+    // Write the bindings
     let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
     bindings.write_to_file(out_path.join("bindings.rs"))
         .expect("Failed to write bindings");
